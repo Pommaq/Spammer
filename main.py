acb1c37bff0c3deb6f75125d267e74740ddaffca
Pommaq <pommaq@protonmail.com>
2022-04-20 23:04:56 +0200

    Formatted file

--- main.py
+++ main.py
@@ -8,7 +8,12 @@ import requests
 domain = "hjalposs47.shop"
 path = "/verification/"
 
-emails = ("gmail.com", "outlook.com", "yahoo.com", "protonmail.com",)
+emails = (
+    "gmail.com",
+    "outlook.com",
+    "yahoo.com",
+    "protonmail.com",
+)
 """
 email=MinEnormaSnopp@gmail.com
 pass=Storpenis1234
@@ -23,22 +28,23 @@ def format_bytes(size) -> typing.Tuple[int, str]:
     """
     power = 2**10
     n = 0
-    power_labels = {0 : '', 1: 'kilo', 2: 'mega', 3: 'giga', 4: 'tera'}
+    power_labels = {0: "", 1: "kilo", 2: "mega", 3: "giga", 4: "tera"}
     while size > power:
         size /= power
         n += 1
-    return size, power_labels[n]+'bytes'
+    return size, power_labels[n] + "bytes"
 
 
 class sender:
     def __init__(self):
         self.cntr = 0
-        self.mtx  = threading.Lock()
+        self.mtx = threading.Lock()
         self.sent = 0
 
-
     def send(self, email, password: str, domain: str) -> requests.Response:
-        x = requests.post(domain, data={"email": email, "pass": password, "get": "Logga in"})
+        x = requests.post(
+            domain, data={"email": email, "pass": password, "get": "Logga in"}
+        )
         self.mtx.acquire()
         self.cntr += 1
         self.sent += len(email) + len(password)
@@ -49,13 +55,11 @@ class sender:
         return x
 
 
-
 with open("firstnames.txt", "r") as file_h:
     firstnames = [x.strip().lower() for x in file_h.readlines()]
 
 with open("lastnames.txt", "r") as file_h:
     lastnames = [x.strip().lower() for x in file_h.readlines()]
-
 
 
 fullpath = f"http://{domain}{path}"
@@ -68,22 +72,18 @@ with open("passlist.txt", "r") as file_h:
 
 reqhandle = sender()
 
+
 def runner():
-    #massivedata = b"A" * (15 * 1024 ** 2)  # 1MB
+    # massivedata = b"A" * (15 * 1024 ** 2)  # 1MB
     while not exiting:
         indx = random.randint(0, len(emails) - 1)
         email = f"{firstnames[random.randint(0, len(firstnames) - 1)]}.{lastnames[random.randint(0, len(lastnames) - 1)]}@{emails[indx]}"
-        password = passwords[random.randint(0, len(passwords) - 1)] #massivedata
+        password = passwords[random.randint(0, len(passwords) - 1)]  # massivedata
         x = reqhandle.send(email, password, fullpath)
         if 400 <= x.status_code <= 600:
             break
 
         time.sleep(0.3)
-
-
-
-
-
 
 
 threads = 4
