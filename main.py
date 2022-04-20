814fac9358f286353c90132383469b6513e0e64f
Timothy Hjort <timothy.hjort@outlook.com>
2022-04-20 15:22:20 +0200

    Use more threads

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import random
 import time
+import threading
+import typing
 
 import requests
 
@@ -11,23 +13,60 @@ emails = ("gmail.com", "outlook.com", "yahoo.com", "protonmail.com",)
 email=MinEnormaSnopp@gmail.com
 pass=Storpenis1234
 get=Logga+in
-
-
 """
+
+class sender:
+    def __init__(self):
+        self.cntr = 0
+        self.mtx  = threading.Lock()
+
+
+    def send(self, email, password: str, domain: str):
+        requests.post(domain, data={"email": email, "pass": password, "get": "Logga in"})
+        self.mtx.acquire()
+        print(f"Sent request no: {self.cntr}")
+        self.cntr += 1
+        self.mtx.release()
+
+
+
 with open("firstnames.txt", "r") as file_h:
-    firstnames = file_h.readlines()
+    firstnames = [x.strip().lower() for x in file_h.readlines()]
 
 with open("lastnames.txt", "r") as file_h:
-    lastnames = file_h.readlines()
+    lastnames = [x.strip().lower() for x in file_h.readlines()]
+
 
 
 fullpath = f"http://{domain}{path}"
-cntr = 0
+
+
+exiting = False
 with open("passlist.txt", "r") as file_h:
-    for passw in file_h:
+    passwords = [x.strip() for x in file_h.readlines()]
+
+
+reqhandle = sender()
+
+def runner():
+    while not exiting:
+
         indx = random.randint(0, len(emails) - 1)
-        email = f"{firstnames[random.randint(0, len(firstnames)- 1)].strip().lower()}.{lastnames[random.randint(0, len(lastnames)- 1)].strip().lower()}@{emails[indx]}"
-        x = requests.post(fullpath, data={"email": email, "pass": passw.strip(), "get": "Logga in"})
-        print(f"Sent request no: {cntr}")
-        cntr += 1
+        email = f"{firstnames[random.randint(0, len(firstnames) - 1)]}.{lastnames[random.randint(0, len(lastnames) - 1)]}@{emails[indx]}"
+        password = passwords[random.randint(0, len(passwords) - 1)]
+        reqhandle.send(email, password, fullpath)
         time.sleep(0.3)
+
+
+
+
+threads = 3
+handles = []
+for _ in range(threads):
+    handle = threading.Thread(target=runner)
+    handle.start()
+    handles.append(handle)
+
+
+for handle in handles:
+    handle.join()
